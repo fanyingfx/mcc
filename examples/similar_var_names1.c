f24c6317d226edec3dbebcdfd996b2e264e8e766
fanyingfx <fanyingfx@outlook.com>
2025-05-01 14:53:17 +0800

    chapter 7, COMPOUND STATEMENTS

---
+++ examples/similar_var_names1.c
@@ -0,0 +1,39 @@
+int main(void) {
+  int a; // a0
+  int result;
+  int a1 = 1; // a10
+  {
+    int a = 2;  // a1
+    int a1 = 2; // a11
+    {
+        
+      int a; // a4
+      {
+        int a; // a5
+        {
+          int a; // a6
+          {
+            int a; // a7
+            {
+              int a; // a8
+              {
+                int a; // a9
+                {
+                  int a = 20; // a10
+                  result = a;
+                  {
+                    int a; // a11
+                    a = 5;
+                    result = result + a;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    result = result + a1;
+  }
+  return result + a1;
+}
