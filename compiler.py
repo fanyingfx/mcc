de7d03987981f97f62e14026f694cdd865ca6e1a
fanyingfx <fanyingfx@outlook.com>
2025-05-17 22:34:50 +0800

    remove `os` using Path.unlink to remove  intermediate files
    update .gitignore

--- compiler.py
+++ compiler.py
@@ -2,35 +2,58 @@
 import subprocess
 import sys
 from pathlib import Path
-import os
-def main():
+
+
+def compile():
     cfile = None
     stage = "--compile"
     for arg in sys.argv[1:]:
-        if arg.endswith('c'):
-            cfile=Path(arg)
+        if arg.endswith("c"):
+            cfile = Path(arg)
         if arg.startswith("--"):
-            stage=arg
-    preprocessed_src = str(cfile.with_suffix(".i"))
-    asm_file= str(cfile.with_suffix(".asm"))
-    obj_file= str(cfile.with_suffix(".o"))
-    exe_name=str(cfile.with_suffix(""))
+            stage = arg
+    preprocessed_src = cfile.with_suffix(".i")
+    asm_file = cfile.with_suffix(".asm")
+    obj_file = cfile.with_suffix(".o")
+    exe_name = str(cfile.with_suffix(""))
 
-    #run preprocesser
-    subprocess.run(["gcc","-E","-P",cfile,"-o",preprocessed_src])
+    # run preprocesser
+    subprocess.run(["gcc", "-E", "-P", cfile, "-o", str(preprocessed_src)])
 
-    #compile to fasm
-    subprocess.run(["moon","run", "-C","/home/fan/code/moonbit/mcc/","src/main","--",preprocessed_src,stage],check=True)
-
+    # compile to fasm
+    subprocess.run(
+        [
+            "moon",
+            "run",
+            "-C",
+            "/home/fan/code/moonbit/mcc/",
+            "src/main",
+            "--",
+            str(preprocessed_src),
+            stage,
+        ],
+        check=True,
+    )
 
     if stage == "--compile":
-        subprocess.run(["fasm",asm_file],check=True)
-        subprocess.run(["ld",str(obj_file),"-dynamic-linker","/usr/lib/ld-linux-x86-64.so.2" ,"-lc" , "-o",exe_name],stdout=subprocess.DEVNULL,check=True)
-    # if Path(asm_file).exists():
-    #     os.remove(asm_file)
-    if Path(preprocessed_src).exists():
-        os.remove(preprocessed_src)
+        subprocess.run(["fasm", str(asm_file)], check=True)
+        subprocess.run(
+            [
+                "ld",
+                str(obj_file),
+                "-dynamic-linker",
+                "/usr/lib/ld-linux-x86-64.so.2",
+                "-lc",
+                "-o",
+                exe_name,
+            ],
+            stdout=subprocess.DEVNULL,
+            check=True,
+        )
+    preprocessed_src.unlink(True)
+    asm_file.unlink(True)
+    obj_file.unlink(True)
 
 
 if __name__ == "__main__":
-    main()
+    compile()
